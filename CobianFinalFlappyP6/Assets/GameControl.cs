990129cf6bd8ae8509da550d9f4070d1cb09ce32
bestatclash <743376@student.dusd.net>
2025-12-12 13:48:27 -0800

    i finishes the game control script
    going to finish the rest of the game

--- CobianFinalFlappyP6/Assets/GameControl.cs
+++ CobianFinalFlappyP6/Assets/GameControl.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameControl : MonoBehaviour
+public class GameControl : MonoBehaviour 
 {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake ()
     {
-        
+        if (instance == null) 
+        { 
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
+    {   
+        if (gameOver == true && Input.GetMouseButtonDown(0)) 
+
+    public void BirdDied()
     {
-        
+        gameOverText.SetActive (true);
+        gameOver = true;
     }
 }
